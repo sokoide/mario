7321229d87ced25a0cb3bcbdb0f902de5d7a2f36
So Koide <sokoide@gmail.com>
2021-08-07 14:35:28 +0900

    feat: add genetic algo

--- mario_ga.py
+++ mario_ga.py
@@ -4,13 +4,31 @@ import numpy as np
 import signal
 import os
 import time
+import pickle
+import copy
+
+# global
+genes = []
+
+# e.g. action = [0, 0, 0, 1, 1, 0]
+# [up, left, down, right, A, B]
+# would activate right (4th element), and A (5th element)
+actions = [
+    [0, 0, 0, 0, 0, 0],  # no input
+    [0, 0, 0, 1, 0, 0],  # right
+    [0, 0, 0, 1, 1, 0],  # right + jump
+    [0, 0, 0, 1, 0, 1],  # right dash
+    [0, 0, 0, 1, 1, 1],  # right dash + jump
+    [0, 0, 0, 0, 1, 0],  # jump
+    [0, 1, 0, 0,  0, 0],  # left
+]
 
 
 def kill_process(path: str):
     for line in os.popen("ps -ef | grep '{}' | grep -v grep".format(path)):
         pid = int(line.split()[1])
-        print(line)
-        print('kill -9 {}'.format(pid))
+        # print(line)
+        # print('kill -9 {}'.format(pid))
         try:
             os.kill(pid, signal.SIGKILL)
         except:
@@ -22,27 +40,10 @@ def clean_fceux():
     kill_process('/opt/homebrew/Cellar/fceux/2.4.0/libexec/fceux')
 
 
-def get_action():
-    # e.g. action = [0, 0, 0, 1, 1, 0]
-    # [up, left, down, right, A, B]
-    # would activate right (4th element), and A (5th element)
-
-    actions = [
-        [0, 0, 0, 0, 0, 0],  # no input
-        [0, 0, 0, 1, 0, 0],  # right
-        [0, 0, 0, 1, 1, 0],  # right + jump
-        [0, 0, 0, 1, 0, 1],  # right dash
-        [0, 0, 0, 1, 1, 1],  # right dash + jump
-        [0, 0, 0, 0, 1, 0],  # jump
-        [0, 1, 0, 0,  0, 0],  # left
-    ]
-    return actions[np.random.randint(0, len(actions))]
-
-
 def make_random_gene(steps):
     gene = []
     for _ in range(steps):
-        gene.append(get_action())
+        gene.append(np.random.randint(0, len(actions)))
     return gene
 
 
@@ -50,13 +51,13 @@ def play_mario(env, gene):
     FRAMES = 4
     score = 0
     observation = env.reset()
-    print('reset')
 
-    for action in gene:
+    for action_id in gene:
         for i in range(FRAMES):
             # action = np.random.randint(0, 1+1, 6)
             # action = [0, 0, 0, 1, 0, 1]
             # action = get_action()
+            action = actions[action_id]
             observation, reward, done, info = env.step(action)
             # print("action=", action)
             # print("observation=", observation)
@@ -65,20 +66,97 @@ def play_mario(env, gene):
             # print("info=", info)
             score = max(score, info['distance'])
             if done:
-                print('done')
                 return score
     return score
 
 
+def print_generation_result(gen: int):
+    l = sorted(genes, key=lambda k: k['score'])
+    first = l[-1]
+    print('* best score: {}'.format(first['score']))
+    # print('* genes: {}'.format(genes))
+
+
+def change_generation():
+    global genes
+
+    l = sorted(genes, key=lambda k: k['score'])
+    first = l[-1]
+    second = l[-2]
+
+    new_genes = []
+
+    # keep the best and 2nd ones
+    new_genes.append(first)
+    new_genes.append(second)
+
+    # mutate the best one
+    new_gene = copy.deepcopy(first['gene'])
+    for i in range(0, len(new_gene)//100):
+        r = np.random.randint(0, len(new_gene))
+        new_gene[r] = np.random.randint(0, len(actions))
+    new_genes.append({'gene': new_gene, 'score': 0})
+
+    # cross over the 1st and 2nd
+    for i in range(len(genes)-len(new_genes)):
+        p = np.random.randint(0, len(first['gene'])//2)
+        # s1 = genes[np.random.randint(0, len(genes))]
+        # s2 = genes[np.random.randint(0, len(genes))]
+        s1 = first
+        s2 = second
+        new_gene = s1['gene'][:p] + s2['gene'][p:]
+        new_genes.append({'gene': new_gene, 'score': 0})
+
+    # mutete from the 3rd one to the last
+    for i in range(3, len(new_genes)):
+        gene = new_genes[i]['gene']
+        for j in range(0, len(gene)//100):
+            r = np.random.randint(0, len(gene))
+            gene[r] = np.random.randint(0, len(actions))
+    genes = new_genes
+    # print('* new genes: {}'.format(genes))
+
+
+def save_genes(gen):
+    filepath = 'gene%04d.pkl' % gen
+    with open(filepath, 'wb') as tf:
+        pickle.dump(genes, tf)
+
+
+def load_genes(gen):
+    global genes
+
+    filepath = 'gene%04d.pkl' % gen
+    with open(filepath, 'rb') as tf:
+        genes = pickle.load(tf)
+
+
 def main():
-    for episode in range(5):
-        # recreate env every episode to avoid 5-7 second dealy at start up after Mario is killed by Kuribo
-        env = gym.make('ppaquette/SuperMarioBros-1-1-v0')
-        gene = make_random_gene(100)
-        score = play_mario(env, gene)
-        print('score: {}'.format(score))
-        clean_fceux()
-        env.close()
+    global genes
+
+    NUM_GENES = 10
+    gen = 1
+
+    for i in range(NUM_GENES):
+        genes.append({'gene': make_random_gene(1000), 'score': 0})
+
+    while True:
+        print('* generation: {}'.format(gen))
+        for gene in genes:
+            # the best one is not mutated and already has the score
+            if gene['score'] > 0:
+                print('result: {}'.format(gene['score']))
+                continue
+            # recreate env every episode to avoid 5-7 second dealy at start up after Mario is killed by Kuribo
+            env = gym.make('ppaquette/SuperMarioBros-1-1-v0')
+            gene['score'] = play_mario(env, gene['gene'])
+            print('result: {}'.format(gene['score']))
+            clean_fceux()
+            env.close()
+        print_generation_result(gen)
+        save_genes(gen)
+        change_generation()
+        gen += 1
 
 
 if __name__ == '__main__':
